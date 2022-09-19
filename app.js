70f0fe1cc524e877bcd1b9d075e69ee08e6ed81e
Ahmed Hanafy <ahmedhanafyr99@gmail.com>
2022-09-19 11:20:39 -0700

    Solo Sprint Complete

--- app.js
+++ app.js
@@ -1,1 +1,44 @@
 // TODO
+const {useState} = React;
+
+const App = () => {
+  return (
+    <div>
+      <h1>Grocery List</h1>
+      <GroceryList listOfItems={['Cucumber', 'Kale', 'Peaches']}/>
+    </div>
+  )
+}
+
+
+
+const GroceryList = (props) => {
+  return (
+    <ul>
+      {props.listOfItems.map((item) => (
+        <GroceryListItem item ={item} />
+      ))}
+    </ul>
+  )
+}
+
+const GroceryListItem = (props) => {
+  const [isDone, setIsDone] = useState(false);
+  const [isHovered, setIsHovered] = useState(false);
+
+  const style = {
+    textDecoration: isDone ? 'line-through' : 'none',
+    fontWeight: isHovered? 'bold' : 'normal'
+  };
+
+  return (
+    <li style ={style} onClick = {() => setIsDone(!isDone)} onMouseEnter = {() => setIsHovered(!isHovered)} onMouseLeave = {() => setIsHovered(!isHovered)}>
+      {props.item}
+    </li>
+
+  )
+}
+
+
+
+ReactDOM.render(<App />, document.getElementById('app'));
